7a0ebdc49d865ed03fa653dc59801e0400ac192e
yashwanth <yashwanth42078@gmail.com>
2021-06-10 22:39:35 +0530

    Added backend code for login page it's working fine with db

--- frontEnd/e-learning/api/app.py
+++ frontEnd/e-learning/api/app.py
@@ -1,32 +1,58 @@
 from types import resolve_bases
-from flask import Flask, request, Response
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+from flask import Flask, request, Response, jsonify, make_response
+
 
 app = Flask(__name__)
 
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'EmpData'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '2571999'
+app.config['MYSQL_DB'] = 'elearning'
+  
+mysql = MySQL(app)
 
-# @app.route('/', defaults={'path':''})
+
+@app.route('/hello_world', methods = ['GET', 'POST'])
+def hello_world():
+    return make_response(jsonify(
+            message = "Hello World from Yashwanth"
+        ), 200)
+
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    print(request.args)
     params = request.args
-    conn = mysql.connect()
-    cursor =conn.cursor()
-    cursor.execute('''select * from user where Email = %s''',(params.get("email")))
-    data = cursor.fetchall()
-    response = Response()
-    response.status_code = 200
-    response.data = "This is from backend"
-    return response
+    email = params.get("email")
+    password = params.get("password")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('select Password from user where Email = %s',(email, ))
+    data = cursor.fetchone()
+    cursor.close
+    if(len(data) == 0):
+        return make_response(jsonify(
+            message = "Didn't find any account associated with this EmailID, Please create a new account"
+        ), 200)
+    elif(password != data['Password']):
+        return make_response(jsonify(
+            message = "Password didn't match, Please try again"
+        ), 200)
+    return make_response(jsonify(
+            message = "Successfully logged in"
+        ), 200)
 
 @app.route('/register', methods = ['POST', 'GET'])
 def register():
+    params = request.args
+    email = params.get("email")
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('select Password from user where Email = %s',(email, ))
+    result = cursor.fetchone
+    if(len(result) != 0):
+        return make_response(jsonify(
+            message = "Account already exits with this EmailID, try logging in"
+        ), 200)
+    
     print(request.args)
     print(request.data)
     print(request.path)
