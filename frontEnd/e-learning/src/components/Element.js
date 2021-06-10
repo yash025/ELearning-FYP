b128f8bb71f542c105ebf9851c11c06df4539469
yashwanth <yashwanth42078@gmail.com>
2021-06-10 23:34:40 +0530

    renaming a folder

--- frontEnd/e-learning/src/components/Element.js
+++
@@ -1,8 +0,0 @@
-import React,{Component} from 'react';
-import './Element.css';
-export default class Element extends Component{
-    
-    render(){
-        return  <div style= {{backgroundColor: this.props.color}} onClick={this.props.click} className="ElementClass"><p>{this.props.number}</p></div>;
-    }
-}
