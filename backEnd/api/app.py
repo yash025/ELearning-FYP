ad3c6bd2c13bf57280be723b75a685dc9b268e31
yashwanth <yashwanth42078@gmail.com>
2021-06-20 22:23:26 +0530

    Implementing /canvas endpoint

--- backEnd/api/app.py
+++ backEnd/api/app.py
@@ -2,6 +2,11 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from flask import Flask, request, jsonify, make_response
 from flask_cors import cross_origin
+import base64
+from keras.models import load_model
+import cv2
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -233,12 +238,83 @@ def fetchRanks():
 @cross_origin()
 def doodleRecognition():
     params = request.args
-    email = params.get('email')
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     type = params.get('type')
+    img_data = params.get('dataUrl')
+    img_width = 28
+    img_height = 28
+    filename = 'C:/Users/Yashwanth/Documents/doodle.png' 
+    with open(filename, 'wb') as f:
+        f.write(base64.b64decode(img_data))
+    img = cv2.imread(filename, 0)
+    img = cv2.resize(img, (img_width, img_height))
+    arr = np.array(img-255)
+    arr = np.array(arr/255.)
+    new_test_cnn = arr.reshape(1, 28, 28, 1).astype('float32')
     if(type == "learning"):
-        
-  
+        category = params.get('category')
+        if(category == 'alphabets'):
+            model = load_model('C:/Users/Yashwanth/Documents/Project/Elearning-FYP/backEnd/api/Alphabet_Recognition/alphabetRecognition.model')
+            label_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',
+            13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X',24:'Y',25:'Z'}
+            cnn_probab = model.predict(new_test_cnn, batch_size=32, verbose=0)
+            pr = model.predict_classes(arr.reshape((1, 28, 28, 1)))
+            expected = params.get('selected')
+            actual = label_dict[pr]
+            if(actual == expected):
+                return make_response(jsonify(
+                        result = "Correct"
+                    ), 200)
+            else:
+                return make_response(jsonify(
+                        result = "Not Correct"
+                    ), 400)
+        elif(category == "digits"):
+            model = load_model('C:/Users/Yashwanth/Documents/Project/Elearning-FYP/backEnd/api/Digit_Recognition/digitRecognition.model')
+            label_dict = {0:'One',1:'Two',2:'Three',3:'Four',4:'Five',5:'Six',6:'Seven',7:'Eight',8:'Nine'}
+            cnn_probab = model.predict(new_test_cnn, batch_size=32, verbose=0)
+            pr = model.predict_classes(arr.reshape((1, 28, 28, 1)))
+            expected = params.get('selected')
+            actual = label_dict[pr]
+            if(actual == expected):
+                return make_response(jsonify(
+                        result = "Correct"
+                    ), 200)
+            else:
+                return make_response(jsonify(
+                        result = "Not Correct"
+                    ), 400)
+        else:
+            model = load_model('C:/Users/Yashwanth/Documents/Project/Elearning-FYP/backEnd/api/Doodle_Recognition/quickdraw.model')
+            label_dict = {0:"apple", 1:"arm", 2:"banana", 3:"bandage", 4:"baseball", 5:"bat", 6:"blackberry", 7:"bread", 8:"candle", 
+            9:"carrot", 10:"circle", 11:"cloud", 12:"diamond", 13:"door", 14:"elbow", 15:"feather", 16:"foot", 17:"frying pan", 18:"hat", 
+            19:"hexagon", 20:"hockey stick", 21:"key", 22:"knife", 23:"leaf", 24:"octagon", 25:"paintbrush", 26:"star", 27:"triangle", 28:"zigzag"}
+            cnn_probab = model.predict(new_test_cnn, batch_size=32, verbose=0)
+            pr = model.predict_classes(arr.reshape((1, 28, 28, 1)))
+            expected = params.get('selected')
+            actual = label_dict[pr]
+            if(actual == expected):
+                return make_response(jsonify(
+                        result = "Correct"
+                    ), 200)
+            else:
+                return make_response(jsonify(
+                        result = "Not Correct"
+                    ), 400)
+    else:
+        model = load_model('C:/Users/Yashwanth/Documents/Project/Elearning-FYP/backEnd/api/Digit_Recognition/digitRecognition.model')
+        label_dict = {0:'One',1:'Two',2:'Three',3:'Four',4:'Five',5:'Six',6:'Seven',7:'Eight',8:'Nine'}
+        cnn_probab = model.predict(new_test_cnn, batch_size=32, verbose=0)
+        pr = model.predict_classes(arr.reshape((1, 28, 28, 1)))
+        expected = params.get('selected')
+        actual = label_dict[pr]
+        if(actual == expected):
+            return make_response(jsonify(
+                    result = "Correct"
+                ), 200)
+        else:
+            return make_response(jsonify(
+                    result = "Not Correct"
+                ), 400)
 
 if __name__ == '__main__':
     app.run(host = "localhost", port = 5000, debug = True)
